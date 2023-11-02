47761c0e2c46973ce194f191a4ff3ccc8e0c4ba0
Christoph Gödecke <cgodecke@student.42wolfsburg.de>
2023-11-02 19:30:40 +0100

    added ex00

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 11:01:37 by cgodecke          #+#    #+#             */
+/*   Updated: 2023/11/02 19:26:32 by cgodecke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat
+{
+	private:
+		std::string	_name;
+		int			_grade;
+
+	public:
+		// Constructors
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat &other);
+		Bureaucrat &operator=(const Bureaucrat &other);
+		~Bureaucrat();
+
+		// Member functions
+		std::string	getName() const;
+		int			getGrade() const;
+		void		incrementGrade();
+		void		decrementGrade();
+
+		// Exceptions
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+
+				virtual const char *what() const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream & os, Bureaucrat const &other);
